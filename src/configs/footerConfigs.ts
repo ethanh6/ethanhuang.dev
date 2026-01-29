91fd75f8c312591bed408881c1a004d882bc49f4
EthanHuang <ethanhuang0606@gmail.com>
2026-01-29 12:22:43 -0800

    update email

--- src/configs/footerConfigs.ts
+++ src/configs/footerConfigs.ts
@@ -1,7 +1,7 @@
 export const footerConfigs = {
   credit: 'Ethan Huang',
   socialLinks: {
-    email: 'ethan@ethanhuang.dev',
+    email: 'eh@ethanhuang.dev',
     github: 'https://github.com/ethanh6',
     linkedin: 'https://www.linkedin.com/in/ethanhuang0606/',
   },
